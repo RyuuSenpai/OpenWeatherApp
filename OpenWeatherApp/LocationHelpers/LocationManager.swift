3663f16b861d3a9f002758a8ee438bca1651195e
Eslam Abo Elfetouh <eslamaboelfetouh@gmail.com>
2023-03-18 12:17:26 +0200

    Update LocationManager Class

--- OpenWeatherApp/LocationHelpers/LocationManager.swift
+++ OpenWeatherApp/LocationHelpers/LocationManager.swift
@@ -8,17 +8,42 @@
 import Foundation
 import CoreLocation
 
+enum LocationAuthorizationStatus {
+    case notDetermined
+    case denied
+    case authorized
+}
+
+protocol LocationAuthorizationDelegate: AnyObject {
+    func authorizationStatusChanged(status: LocationAuthorizationStatus)
+}
+
 protocol LocationManagerDelegate: AnyObject {
     func getCurrentLocation(completion: @escaping (CLLocation?) -> Void)
+    func requestAuthorization()
 }
+
 final class LocationManager: NSObject, LocationManagerDelegate {
-    
+
     static let shared = LocationManager()
-    
+
     private let locationManager = CLLocationManager()
     private var locationCompletionHandler: ((CLLocation?) -> Void)?
     private var userLastLocation: CLLocation?
     private var didUpdateLocation = false
+    weak var authorizationDelegate: LocationAuthorizationDelegate?
+
+    var authorizationStatus: LocationAuthorizationStatus {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways:
+            return .authorized
+        case .denied:
+            return .denied
+        default:
+            return .notDetermined
+        }
+    }
+
     private override init() {
         super.init()
         locationManager.delegate = self
@@ -28,28 +53,40 @@ final class LocationManager: NSObject, LocationManagerDelegate {
         locationManager.stopUpdatingLocation()
     }
 
+    func requestAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
     func getCurrentLocation(completion: @escaping (CLLocation?) -> Void) {
         locationCompletionHandler = completion
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+        switch authorizationStatus {
+        case .authorized:
+            locationManager.startUpdatingLocation()
+        case .denied:
+            locationCompletionHandler?(nil)
+        case .notDetermined:
+            requestAuthorization()
+        }
     }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last,
-        !didUpdateLocation else { return }
+              !didUpdateLocation else { return }
         userLastLocation = location
-        didUpdateLocation = true 
+        didUpdateLocation = true
         locationCompletionHandler?(location)
         locationManager.stopUpdatingLocation()
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         debugPrint("Failed to get user location: \(error.localizedDescription)")
         locationCompletionHandler?(nil)
         locationManager.stopUpdatingLocation()
     }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationDelegate?.authorizationStatusChanged(status: authorizationStatus)
+    }
 }
-
-
